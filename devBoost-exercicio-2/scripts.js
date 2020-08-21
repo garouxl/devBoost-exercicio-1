e4e3f7c08c9812cb3b7ce8f8958e0bd615457d24
Erikson Gonçalves <eriksongoncalves@yahoo.com.br>
2020-08-21 18:45:33 -0300

    fix: corrigindo seletor

--- devBoost-exercicio-2/scripts.js
+++ devBoost-exercicio-2/scripts.js
@@ -48,7 +48,7 @@ function stopCounter(timerRef, btnRef, inputRef) {
 
 function insertTask(description) {
   var listTask = document.querySelector(".list-task");
-  var listTaskItems = document.querySelector(
+  var listTaskItems = document.querySelectorAll(
     ".list-task:not(.result) .list-task-item"
   );
   var result = document.querySelector(".list-task.result");
@@ -65,7 +65,7 @@ function insertTask(description) {
 
   listTask.appendChild(child);
 
-  if (!listTaskItems) {
+  if (listTaskItems.length === 0) {
     result.style.display = "block";
   }
 }
