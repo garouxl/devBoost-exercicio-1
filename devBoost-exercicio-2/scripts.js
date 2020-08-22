1d1c356da03e806a2ae54ac832470d5e3709bed6
GabiLea <gabriela.leandro.ti@gmail.com>
2020-08-22 01:49:52 -0300

    Somar horas
    Subindo função de somar as horas

--- devBoost-exercicio-2/scripts.js
+++ devBoost-exercicio-2/scripts.js
@@ -12,9 +12,10 @@ function formatTime(value) {
 }
 
 function startCounter(inputRef) {
+  var inputHour = inputRef.parentNode.querySelector("input[name=task-hour");
+
   var timerId = setInterval(
     function () {
-      var inputHour = inputRef.parentNode.querySelector("input[name=task-hour");
       var btn = inputRef.parentNode.querySelector("button");
       var hour = inputHour.value.split(":");
       var d = new Date();
@@ -36,6 +37,9 @@ function startCounter(inputRef) {
     }.bind(this),
     1000
   );
+
+  inputHour.setAttribute('data-interval', timerId);
+
 }
 
 function stopCounter(timerRef, btnRef, inputRef) {
@@ -70,7 +74,60 @@ function insertTask(description) {
   }
 }
 
+function calculate() {
+  var inputTime = document.querySelectorAll(
+    ".list-task:not(.result) .list-task-item input[name=task-hour"
+  );
+
+  var sum = "";
+
+  for (var i = 0; i < inputTime.length; i++) {
+    var timerRef = parseInt(inputTime[i].getAttribute("data-interval"));
+    var btn = inputTime[i].parentNode.querySelector("button");
+
+    //para todos os cronometros.
+    stopCounter(timerRef, btn, inputTime[i]);
+
+    if (i != 0) {
+      var hourA = sum;
+      var hourB = inputTime[i].value;
+
+      sum = sumHour(hourA, hourB);
+
+    } else {
+      sum = inputTime[i].value;
+    }
+    document.getElementById("valueCalculate").value = sum;
+  }
+}
+
+function sumHour(hourA, hourB) {
+  hourA = hourA.split(':');
+  hourB = hourB.split(':');
+  hourTotal = parseInt(hourA[0], 10) + parseInt(hourB[0], 10);
+  minTotal = parseInt(hourA[1], 10) + parseInt(hourB[1], 10);
+  segTotal = parseInt(hourA[2], 10) + parseInt(hourB[2], 10);
+
+  if (segTotal >= 60) {
+    segTotal -= 60; minTotal += 1;
+  }
+
+  if (minTotal >= 60) {
+    minTotal -= 60; hourTotal += 1;
+  }
+
+  hourTotal = ("00" + hourTotal).slice(-2);
+  minTotal = ("00" + minTotal).slice(-2);
+  segTotal = ("00" + segTotal).slice(-2);
+
+  result = hourTotal + ":" + minTotal + ":" + segTotal;
+  return result;
+}
+
 window.addEventListener("load", function () {
   var btnAddTask = document.getElementById("btnAddTask");
+  var btnCalculate = document.getElementById("btnCalculate");
+
   btnAddTask.addEventListener("click", addTask);
+  btnCalculate.addEventListener("click", calculate);
 });
