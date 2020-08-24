248f1281a7c0732ebccb88a2144956b633e44922
Erikson Gonçalves <eriksongoncalves@yahoo.com.br>
2020-08-23 21:55:53 -0300

    Mostrando e escondendo saldos

--- devBoost-interface-itau-2.0/js/main.js
+++ devBoost-interface-itau-2.0/js/main.js
@@ -10,3 +10,18 @@ function toogleCardInfo(element) {
     element.textContent = "expandir";
   }
 }
+
+function toogleSaldo() {
+  var saldoContainer = document.querySelector(".saldo-container");
+  var values = saldoContainer.querySelectorAll(".saldo");
+
+  if (values.length > 0) {
+    for (i = 0; i < values.length; i++) {
+      if (values[i].classList.contains("hide")) {
+        values[i].classList.remove("hide");
+      } else {
+        values[i].classList.add("hide");
+      }
+    }
+  }
+}
